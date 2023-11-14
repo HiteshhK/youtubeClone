a0202ae671c93130cba57465e7d068cbff51938a
Hitesh Kaushik <kaushikhitesh002@gmail.com>
2023-11-14 12:25:13 +0530

    keys handling

--- src/utils/keys.js
+++
@@ -1,3 +0,0 @@
-const GOOGLE_API_KEY="AIzaSyB5ZXgQb-J173p1ZewPeeMMiyPopTRF938";
-
-export const YOUTUBE_VIDEOS_API="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
